079408adda38ce6abc388ff706bd0797528cbdbe
sumory <sumory.wu@gmail.com>
2016-03-01 14:44:22 +0800

    fixbug: no header error

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ this is a upload middleware for `lor`, it depends on the "uuid" module.
 usage example in lor:
 
 ```lua
-local uploader_middleware = require("app.middleware.uploader")
-app:use(uploader_middleware({
+local upload_middleware = require("app.middleware.upload")
+app:use(upload_middleware({
     dir = "app/static/avatar" -- static files path
 }))
 ``` 
--- upload.lua
+++ upload.lua
@@ -19,15 +19,19 @@ local function _multipart_formdata(config)
 		ngx.exit(500)
 	end
 	form:set_timeout(config.recieve_timeout)
+		
 
 	local unique_name = uuid()
-	local file, origin_filename, filename, path, extname
+	local success, msg = false, ""
+	local file, origin_filename, filename, path, extname, err
 	while true do
 		local typ, res, err = form:read()
 
 		if not typ then
+			success = false
+			msg = "failed to read"
 			ngx.log(ngx.ERR, "failed to read: ", err)
-			ngx.exit(500)
+			return success, msg
 		end
 
 		if typ == "header" then
@@ -43,17 +47,35 @@ local function _multipart_formdata(config)
 					extname = getextension(origin_filename)
 				end
 
+				if extname ~= "png" and extname ~= "jpg" and extname ~= "jpeg"  and extname ~= "bmp"  and extname ~= "gif" then
+					success = false
+					msg = "not allowed upload file type"
+					ngx.log(ngx.ERR, "not allowed upload file type:", origin_filename)
+					return success, msg
+				end  
+
 				filename = unique_name .. "." .. extname
 				path = config.dir.. "/" .. filename
+			
 				
-				file = io.open(path, "w+")
+				file, err = io.open(path, "w+")
+				if err then
+					success = false
+					msg = "open file error"
+					ngx.log(ngx.ERR, "open file error:", err)
+					return success, msg
+				end
 			end
 	
 		elseif typ == "body" then
 			if file then
 				file:write(res)
+				success = true
 			else
-				ngx.log(ngxERR, path, " not exist")
+				success = false
+				msg = "upload file error"
+				ngx.log(ngx.ERR, "upload file error, path:", path)
+				return success, msg
 			end
 		elseif typ == "part_end" then
             file:close()
@@ -65,7 +87,7 @@ local function _multipart_formdata(config)
 		end
 	end
 
-	return origin_filename, extname, path, filename
+	return success, msg, origin_filename, extname, path, filename
 end
 
 local function _check_post(config)
@@ -74,16 +96,23 @@ local function _check_post(config)
 	local path = nil
 
 
+	local success, msg = false, ""
+	local origin_filename, extname, path, filename
 	if ngx_var.request_method == "POST" then
 		local header = get_headers['Content-Type']
-		local is_multipart = sfind(header, "multipart")
-		if is_multipart and is_multipart>0 then
-			origin_filename, extname, path, filename= _multipart_formdata(config)
+
+		if header then
+			local is_multipart = sfind(header, "multipart")
+
+			if is_multipart and is_multipart>0 then
+				success, msg, origin_filename, extname, path, filename= _multipart_formdata(config)
+
+			end
 		end
 	end
 
 
-	return origin_filename, extname, path, filename
+	return success, msg, origin_filename, extname, path, filename
 end
 
 local function uploader(config)
@@ -93,12 +122,19 @@ local function uploader(config)
 	config.recieve_timeout = config.recieve_timeout or 20000 -- 20s
 
 	return function(req, res, next)
-		local origin_filename, extname, path, filename = _check_post(config)
-		req.file = req.file or {}
-		req.file.origin_filename = origin_filename
-		req.file.extname = extname
-		req.file.path = path
-		req.file.filename = filename
+		local success, msg, origin_filename, extname, path, filename = _check_post(config)
+		if success then
+			req.file = req.file or {}
+			req.file.success = true
+			req.file.origin_filename = origin_filename
+			req.file.extname = extname
+			req.file.path = path
+			req.file.filename = filename
+		else
+			req.file = req.file or {}
+			req.file.success = false
+			req.file.msg = msg
+		end
 		next()
 	end
 end
